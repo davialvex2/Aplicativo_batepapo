4735e0fde50468f5b22eafbf04539337e768f304
Davi Augusto <davi.miranda0919@gmail.com>
2026-04-13 21:12:34 -0300

    iniciando projeto

---
+++ src/main/java/daviaugusto/example/aplicativo_batepapo/AplicativoBatepapoApplication.java
@@ -0,0 +1,13 @@
+package daviaugusto.example.aplicativo_batepapo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicativoBatepapoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicativoBatepapoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/daviaugusto/example/aplicativo_batepapo/AplicativoBatepapoApplicationTests.java
@@ -0,0 +1,13 @@
+package daviaugusto.example.aplicativo_batepapo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicativoBatepapoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
